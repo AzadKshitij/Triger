c0de6c1030651c7d08c2a8c7f4f570f5bc76459b
Azad Kshitij <azadkshitij08302001@gmail.com>
2020-11-07 16:36:16 +0530

    Add Input Pollint
    To handle multiple input at a time

--- Triger/src/Triger/Platform/Windows/WindowsWindow.h
+++ Triger/src/Triger/Platform/Windows/WindowsWindow.h
@@ -27,6 +27,8 @@ namespace Triger
 		void SetVSync(bool enabled) override;
 		bool IsVSync() const override;
 
+		inline virtual void* GetNativeWindow() const { return m_Window; }
+
 	private:
 		virtual void Init(const WindowProps &props);
 		virtual void Shutdown();
