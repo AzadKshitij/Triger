855104841b5c901b09fdcc029650a18589bdb68d
Azad Kshitij <azadkshitij08302001@gmail.com>
2020-11-11 12:33:55 +0530

    Window Resizing and zoom level fix

--- Triger/src/Triger/Application.h
+++ Triger/src/Triger/Application.h
@@ -31,12 +31,17 @@ namespace Triger
 
 	private:
 		bool OnWindowClose(WindowCloseEvent& e);
-	
+		bool OnWindowResize(WindowResizeEvent& e);
+
 	private:
 		std::unique_ptr<Window> m_Window;
-		ImGuiLayer* m_ImGuiLayer;
+
 		bool m_Running = true;
+		bool m_Minimized = false;
+		
+		ImGuiLayer* m_ImGuiLayer;
 		LayerStack m_LayerStack;
+		
 		float m_LastFrameTime = 0.0f;
 
 	private:
--- Triger/src/Triger/Renderer/Renderer.h
+++ Triger/src/Triger/Renderer/Renderer.h
@@ -12,7 +12,9 @@ namespace Triger {
 	public:
 		static void Init();
 		static void BeginScene(OrthographicCamera& camera);
+		static void OnWindowResize(uint32_t width, uint32_t height);
 		static void EndScene();
+
 
 		static void Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform = glm::mat4(1.0f));
 
