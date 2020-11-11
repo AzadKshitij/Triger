262b2db165ac63fd0838c84824db1c669c6d92e4
Azad Kshitij <azadkshitij08302001@gmail.com>
2020-11-11 11:37:41 +0530

    Reading Shader Data from file

--- Triger/src/Triger/Renderer/Shader.h
+++ Triger/src/Triger/Renderer/Shader.h
@@ -10,10 +10,12 @@ namespace Triger {
 	public:
 		virtual ~Shader() = default;
 
+
 		virtual void Bind() const = 0;
 		virtual void Unbind() const = 0;
 
 		static Shader* Create(const std::string& vertexSrc, const std::string& fragmentSrc);
+		static Shader* Create(const std::string& filepath);
 	};
 
 }
