f271accd98ffb93f99c224fce3b1764a48cb5b60
Azad Kshitij <azadkshitij08302001@gmail.com>
2020-11-14 22:09:39 +0530

    textureed and non textured quads can be rendered with one shader

--- Triger/src/Triger/Platform/OpenGL/OpenGLBuffer.cpp
+++ Triger/src/Triger/Platform/OpenGL/OpenGLBuffer.cpp
@@ -39,6 +39,8 @@ namespace Triger {
 		: m_Count(count)
 	{
 		glCreateBuffers(1, &m_RendererID);
+		/*glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
+		glBufferData(GL_ARRAY_BUFFER, count * sizeof(uint32_t), indices, GL_STATIC_DRAW);*/
 		glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
 		glBufferData(GL_ARRAY_BUFFER, count * sizeof(uint32_t), indices, GL_STATIC_DRAW);
 	}
