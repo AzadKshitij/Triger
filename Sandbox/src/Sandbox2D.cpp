8c396daf6019db69043f9d41e951e34938806840
Azad Kshitij <azadkshitij08302001@gmail.com>
2020-11-16 22:27:21 +0530

    Updated SandBox2D

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -81,9 +81,9 @@ void Sandbox2D::OnUpdate(Triger::Timestep ts)
 	{
 		PROFILE_SCOPE("Renderer Draw");
 		Triger::Renderer2D::BeginScene(m_CameraController.GetCamera());
-		Triger::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+		Triger::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(-45.0f), { 0.8f, 0.2f, 0.3f, 1.0f });
 		Triger::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-		Triger::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture);
+		Triger::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
 		Triger::Renderer2D::EndScene();
 	}
 	// TODO: Add these functions - Shader::SetMat4, Shader::SetFloat4
