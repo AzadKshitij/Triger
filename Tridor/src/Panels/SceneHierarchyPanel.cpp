0b4b38f1664a351367e4f8deb1924716af060412
Azad Kshitij <azadkshitij08302001@gmail.com>
2020-11-29 12:24:29 +0530

    Properties pannel

--- Tridor/src/Panels/SceneHierarchyPanel.cpp
+++ Tridor/src/Panels/SceneHierarchyPanel.cpp
@@ -1,8 +1,10 @@
 #include "SceneHierarchyPanel.h"
 
 #include <imgui/imgui.h>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "Triger/Scene/Components.h"
+
 
 namespace Triger {
 
@@ -25,6 +27,15 @@ namespace Triger {
 				Entity entity{ entityID , m_Context.get() };
 				DrawEntityNode(entity);
 			});
+
+		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered())
+			m_SelectionContext = {};
+
+		ImGui::End();
+
+		ImGui::Begin("Properties");
+		if (m_SelectionContext)
+			DrawComponents(m_SelectionContext);
 
 		ImGui::End();
 	}
@@ -50,5 +61,33 @@ namespace Triger {
 		}
 
 	}
+
+	void SceneHierarchyPanel::DrawComponents(Entity entity)
+	{
+		if (entity.HasComponent<TagComponent>())
+		{
+			auto& tag = entity.GetComponent<TagComponent>().Tag;
+
+			char buffer[256];
+			memset(buffer, 0, sizeof(buffer));
+			strcpy_s(buffer, sizeof(buffer), tag.c_str());
+			if (ImGui::InputText("Tag", buffer, sizeof(buffer)))
+			{
+				tag = std::string(buffer);
+			}
+		}
+
+		if (entity.HasComponent<TransformComponent>())
+		{
+			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+			{
+				auto& transform = entity.GetComponent<TransformComponent>().Transform;
+				ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.1f);
+
+				ImGui::TreePop();
+			}
+		}
+	}
+
 
 }
