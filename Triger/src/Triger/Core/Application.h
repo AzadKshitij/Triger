07da08624c113e3e81c864a6cc870e66dc6b271d
Azad Kshitij <azadkshitij08302001@gmail.com>
2020-12-12 09:41:12 +0530

    Minor Fix

--- Triger/src/Triger/Core/Application.h
+++ Triger/src/Triger/Core/Application.h
@@ -43,7 +43,7 @@ namespace Triger {
 		bool OnWindowClose(WindowCloseEvent& e);
 		bool OnWindowResize(WindowResizeEvent& e);
 	private:
-		std::unique_ptr<Window> m_Window;
+		Scope<Window> m_Window;
 		ImGuiLayer* m_ImGuiLayer;
 		bool m_Running = true;
 		bool m_Minimized = false;
