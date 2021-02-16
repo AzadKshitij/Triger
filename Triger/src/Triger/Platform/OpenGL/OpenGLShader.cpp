adc22ae5f3dbc070025ea573178971428ff0a8c6
Azad Kshitij <azadkshitij08302001@gmail.com>
2021-02-16 14:12:11 +0530

    Added missing 'SetFloat2' in Renderer for completeness

--- Triger/src/Triger/Platform/OpenGL/OpenGLShader.cpp
+++ Triger/src/Triger/Platform/OpenGL/OpenGLShader.cpp
@@ -219,6 +219,14 @@ namespace Triger {
 		UploadUniformFloat(name, value);
 	}
 
+	void OpenGLShader::SetFloat2(const std::string& name, const glm::vec2& value)
+	{
+		TR_PROFILE_FUNCTION();
+
+		UploadUniformFloat2(name, value);
+	}
+
+
 	void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value)
 	{
 		TR_PROFILE_FUNCTION();
